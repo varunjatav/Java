93adedddf56be7f786f5369c1cf28959edcababd
Varun Jatav <mohit.mohit979@gmail.com>
2023-11-20 19:24:49 +0530

    Stack

--- Stack/MergeTwoStackInThird.java
+++ Stack/MergeTwoStackInThird.java
@@ -1,18 +1,15 @@
-package stack;
 import java.util.Scanner;
-
 public class MergeTwoStackInThird {
     public static void main(String[] args) {
         int stk1[] = new int[6];
         int stk2[] = new int[6];
         int stk3[] = new int[stk1.length + stk2.length];
         int Top1 = -1, Top2 = -1, Top3 = -1;
-        int stk1_count = 0, stk2_count = 0;
+        int stk1_count = 0;
         int min = 0, min2 = 0;
-        int pos_stk1 = -1, pos_stk2 = -1;
+        int pos_stk1 = -1 , pos_stk2 = -1;
 
         Scanner obj = new Scanner(System.in);
-        // Scanner obj2 = new Scanner(System.in);
         while (Top1 < stk1.length - 1 && Top2 < stk2.length - 1) {
             Top1++;
             Top2++;
@@ -22,7 +19,6 @@ public class MergeTwoStackInThird {
             stk2[Top2] = obj.nextInt();
         }
         obj.close();
-        //  obj2.close();
 
         min = stk1[stk1.length - 1];
         min2 = stk2[stk2.length - 1];
@@ -30,9 +26,22 @@ public class MergeTwoStackInThird {
         System.out.println("min2 : " + min2);
         System.out.println("--------------------------------------------------------------------------- ");
 
-        while (stk1_count < stk1.length && stk2_count < stk2.length) {
-            Top1 = stk1.length - 1;
-            Top2 = stk2.length - 1;
+        System.out.println("Stack1");
+        // System.out.println(Top1);
+        // System.out.println(Top2);
+        while (Top1 >= 0) {
+            System.out.println(stk1[Top1]);
+            Top1--;
+        }
+        System.out.println("Stack2");
+        while (Top2 >= 0) {
+            System.out.println(stk2[Top2]);
+            Top2--;
+        }
+
+        while (stk1_count < stk1.length && stk1_count < stk2.length) {
+            // Top1 = stk1.length - 1;
+            // Top2 = stk2.length - 1;
             while (Top1 >= 0) {
                 if (min > stk1[Top1] && stk1[Top1] >= 0) {
                     min = stk1[Top1];
@@ -43,7 +52,7 @@ public class MergeTwoStackInThird {
             System.out.println("min : " + min);
             while (Top2 >= 0) {
                 if (min2 > stk2[Top2] && stk2[Top2] >= 0) {
-                    min = stk2[Top2];
+                    min2 = stk2[Top2];
                     pos_stk2 = Top2;
                 }
                 Top2--;
@@ -61,7 +70,18 @@ public class MergeTwoStackInThird {
                 }
             }
             stk1_count++;
-            stk2_count++;
+        }
+        System.out.println("stk1[pos_stk1] : " + stk1[pos_stk1]);
+        System.out.println("stk2[pos_stk2] : " + stk2[pos_stk2]);
+        System.out.println("Stack1");
+        while (Top1 >= 0) {
+            System.out.println(stk1[Top1]);
+            Top1--;
+        }
+        System.out.println("Stack2");
+        while (Top2 >= 0) {
+            System.out.println(stk2[Top2]);
+            Top2--;
         }
 
         System.out.println("Stack3");
