f60b36627215b7f70c82a9a7129d77dd94cfba53
Varun Jatav <mohit.mohit979@gmail.com>
2023-12-22 15:48:59 +0530

    Infix Prefix

---
+++ Stack/Isfix.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+
+public class Isfix {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String Infix = sc.nextLine();
+        char arr[] = new char[Infix.length()];
+        for (int i = 0; i < Infix.length(); i++) {
+            arr[i] = Infix.charAt(i);
+        }
+        sc.close();
+        Isfix obj = new Isfix();
+        obj.func(arr);
+    }
+
+    public void func(char arr[]) {
+        char arr2[] = new char[arr.length];
+        char st[] = new char[arr.length];
+        int i = 0, j = 0, t = -1;
+        char exp = 'a';
+        while (i < arr.length) {
+            if (arr[i] >= 'A' && arr[i] <= 'Z') {
+                arr2[j] = arr[i];
+                j++;
+            } else {
+                if (t == -1) {
+                    t++;
+                    st[t] = arr[i];
+                } else if (t > -1) {
+                    if (arr[i] == '-' && st[t] == '*' || arr[i] == '*' && st[t] == '*' || arr[i] == '/' && st[t] == '/'
+                            || arr[i] == '*' && st[t] == '/' || arr[i] == '-' && st[t] == '/' || arr[i]== '+' && st[t] =='/') {
+                        exp = st[t];
+                        arr2[j] = exp;
+                        j++;
+                        st[t] = arr[i];
+                    } else {
+                        t++;
+                        st[t] = arr[i];
+                    }
+                }
+            }
+            i++;
+        }
+        if (t > -1) {
+            while (t > -1) {
+                arr2[j] = st[t];
+                t--;
+                j++;
+            }
+        }
+        for (int k = 0; k < arr2.length; k++) {
+            System.out.println("arr2 : " + arr2[k]);
+        }
+    }
+}
--- Stack/MergeTwoStackInThird.java
+++ Stack/MergeTwoStackInThird.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class MergeTwoStackInThird {
     public static void main(String[] args) {
         int stk1[] = new int[6];
-        int stk2[] = new int[6];
+        int stk2[] = new int[8];
         int stk3[] = new int[stk1.length + stk2.length];
         int Top1 = -1, Top2 = -1, Top3 = -1;
         int stk1_count = 0;
@@ -58,23 +58,31 @@ public class MergeTwoStackInThird {
                     stk3[Top3] = min;
                     stk1[pos_stk1] = -1;
                     System.out.println("min: " + min );
-                } else if (min2 < min) {
+                } 
+                else if (min2 < min) {
                     stk3[Top3] = min2;
                     stk2[pos_stk2] = -1;
                     System.out.println("min2: " + min2 );
                 }
+                else if(Top1 == 0){
+                    Top3++;
+                    stk3[Top3] = min2;
+                }else if(Top2 == 0){
+                    Top3++;
+                    stk3[Top3] = min;
+                }
            
             stk1_count++;
         }
-        System.out.println("stk1[pos_stk1] : " + stk1[pos_stk1]);
-        System.out.println("stk2[pos_stk2] : " + stk2[pos_stk2]);
+        // System.out.println("stk1[pos_stk1] : " + stk1[pos_stk1]);
+        // System.out.println("stk2[pos_stk2] : " + stk2[pos_stk2]);
         
         System.out.println("Stack3");
-        while (Top3 >= 0) {
-            System.out.println(stk3[Top3]);
-            Top3--;
-        }
-        stk3[stk3.length -1] = min;
+        // while (Top3 >= 0) {
+        //     System.out.println(stk3[Top3]);
+        //     Top3--;
+        // }
+        stk3[stk3.length -1] = min2;
         Top3 = stk3.length -1;
         while (Top3 >= 0) {
             System.out.println(stk3[Top3]);
